3a0d51822abf50dba6fce106a93ed64a837b1d22
juanchovelezpro <velezolaya2012@hotmail.com>
2023-08-31 23:21:12 -0500

    Fixing project

--- src/main/kotlin/com/kemp/plugins/Logging.kt
+++
@@ -1,9 +0,0 @@
-package com.kemp.plugins
-
-import io.ktor.server.application.*
-import io.ktor.server.plugins.callloging.*
-import org.slf4j.event.LoggingEvent
-
-fun Application.configureLogging(){
-    install(CallLogging)
-}

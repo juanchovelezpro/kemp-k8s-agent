38429dea732111d6badebe7b63122fe5ecf915b2
juanchovelezpro <velezolaya2012@hotmail.com>
2024-04-05 19:54:18 -0500

    Only logic to get data from cluster

--- src/main/kotlin/com/kemp/Application.kt
+++ src/main/kotlin/com/kemp/Application.kt
@@ -2,10 +2,9 @@ package com.kemp
 
 import com.kemp.client.KubeClient
 import com.kemp.utils.toJson
-import io.kubernetes.client.util.generic.options.ListOptions
 
 fun main() {
     val k8sClient = KubeClient()
-    println(k8sClient.listObjects("pods", listOptions = ListOptions()))
     println(k8sClient.getServerResources().toJson())
+
 }
